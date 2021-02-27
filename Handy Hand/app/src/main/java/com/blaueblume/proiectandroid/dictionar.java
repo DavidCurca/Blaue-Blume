9d8cbc6a61bef2383db0f81c931557b6ab8ff38d
David Curca <curcadavid2@gmail.com>
2021-02-26 21:01:28 -0800

    first commit

---
+++ Handy Hand/app/src/main/java/com/blaueblume/proiectandroid/dictionar.java
@@ -0,0 +1,14 @@
+package com.blaueblume.proiectandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dictionar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionar);
+    }
+}
